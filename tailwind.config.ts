e04f37b5105a53b2da0a6a8c3794071cbcf7a4b3
Deepak Bagada <dbagada910@gmail.com>
2025-07-21 05:21:07 +0000

    use the comfortaa font style in whoel webstie

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Comfortaa', 'sans-serif'],
       },
       colors: {
         border: 'hsl(var(--border))',
